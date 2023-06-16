17cc10f81d280a2274e1f72904997ca68a410cd6
gamba <iamoldandiam16@gmail.com>
2023-06-16 01:06:12 -0500

    finished hw1

--- homework/homework1/Program.cs
+++ homework/homework1/Program.cs
@@ -3,7 +3,9 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
-/*
+
+Console.WriteLine("EXERCISE 2");
+
 Console.WriteLine("Please, input first number:");
 int num1_1 = Convert.ToInt32(Console.ReadLine());
 
@@ -13,19 +15,21 @@ int num1_2 = Convert.ToInt32(Console.ReadLine());
 if (num1_1 > num1_2) {
         Console.WriteLine($"max = {num1_1}");
         Console.WriteLine($"min = {num1_2}"); 
-} if (num2 > num1) {
+} if (num1_2 > num1_1) {
         Console.WriteLine($"max = {num1_2}");
         Console.WriteLine($"min = {num1_1}"); 
 } else {
     Console.WriteLine($"{num1_1} = {num1_2}");
-}; */
+}; 
 
 /* Задача 4: 
 Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
-/*
+
+Console.WriteLine("EXERCISE 4");
+
 Console.WriteLine("Please, input first number:");
 int num2_1 = Convert.ToInt32(Console.ReadLine());
 
@@ -44,12 +48,14 @@ if (num2_1 > num2_2 && num2_1 > num2_3) {
 } if (num2_1 == num2_2 || num2_2 == num2_3 || num2_3 == num2_1) {
     Console.WriteLine("The numbers need to be different");
 }; 
-*/
+
 /* Задача 6: 
 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
 7 -> нет */
+
+Console.WriteLine("EXERCISE 6");
 
 Console.WriteLine("Please, input your number:");
 int num3 = Convert.ToInt32(Console.ReadLine());
@@ -66,3 +72,17 @@ if (res == 0) {
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
 
+Console.WriteLine("EXERCISE 8");
+
+Console.WriteLine("Please, input your number:");
+int num4 = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num4) {
+    int temp = count % 2;
+    if (temp == 0) {
+    Console.Write($"{count}, ");
+    count = count + 1;
+    } else {
+    count = count + 1; }
+};
