579bf81187fcc59987e1209233eb58f47c4f7346
gamba <iamoldandiam16@gmail.com>
2023-06-16 00:02:58 -0500

    ex1: found square of the set number

--- Seminars/Seminar1/Program.cs
+++ Seminars/Seminar1/Program.cs
@@ -1,8 +1,12 @@
 ﻿
-Console.WriteLine("What is the date?"); //write on line then enter new line
+//Console.WriteLine("What is the date?"); //write on line then enter new line
 // Console.Write("Today is Friday!"); // write on line
 
 // Console.ReadLine(); // - string
-int numA = Convert.ToInt32(Console.ReadLine()); // - int
-Console.WriteLine("Today is " + numA);
-Console.Write($"String is {numA}"); // $" {}" - symbol that allows us to include a number in our string w/o changing the type
+// int numA = Convert.ToInt32(Console.ReadLine()); // - int
+// Console.WriteLine("Today is " + numA);
+// Console.Write($"String is {numA}"); // $" {}" - symbol that allows us to include a number in our string w/o changing the type
+
+int numA = Convert.ToInt32(Console.ReadLine());
+int res = numA * numA;
+Console.WriteLine(res);
