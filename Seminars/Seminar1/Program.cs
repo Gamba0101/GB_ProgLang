8010bda5c50860d2366c91a4fa959048bdf3adeb
gamba <iamoldandiam16@gmail.com>
2023-06-16 00:13:57 -0500

    ex2: checked if input num1 is square of input num2

--- Seminars/Seminar1/Program.cs
+++ Seminars/Seminar1/Program.cs
@@ -7,6 +7,21 @@
 // Console.WriteLine("Today is " + numA);
 // Console.Write($"String is {numA}"); // $" {}" - symbol that allows us to include a number in our string w/o changing the type
 
-int numA = Convert.ToInt32(Console.ReadLine());
-int res = numA * numA;
-Console.WriteLine(res);
+// Exersice 1: find the square of inputted number
+/* int numA = Convert.ToInt32(Console.ReadLine());
+   int res = numA * numA;
+   Console.WriteLine(res); */
+
+// Exercise 2: Get two numbers from user and check if the second number is the square of first number
+
+Console.WriteLine("Please, input first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Please, input second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 == num2 * num2){
+    Console.WriteLine($"{num1} is a square of {num2}"); 
+} else {
+    Console.WriteLine($"{num1} is NOT a square of {num2}"); 
+};
