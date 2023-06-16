52555ac6522f7e85da4fb0d0670a0b62d7dcb761
gamba <iamoldandiam16@gmail.com>
2023-06-16 00:59:16 -0500

    done ex3 of hw

--- homework/homework1/Program.cs
+++ homework/homework1/Program.cs
@@ -5,19 +5,19 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 /*
 Console.WriteLine("Please, input first number:");
-int num1 = Convert.ToInt32(Console.ReadLine());
+int num1_1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Please, input second number:");
-int num2 = Convert.ToInt32(Console.ReadLine());
+int num1_2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2) {
-        Console.WriteLine($"max = {num1}");
-        Console.WriteLine($"min = {num2}"); 
+if (num1_1 > num1_2) {
+        Console.WriteLine($"max = {num1_1}");
+        Console.WriteLine($"min = {num1_2}"); 
 } if (num2 > num1) {
-        Console.WriteLine($"max = {num2}");
-        Console.WriteLine($"min = {num1}"); 
+        Console.WriteLine($"max = {num1_2}");
+        Console.WriteLine($"min = {num1_1}"); 
 } else {
-    Console.WriteLine($"{num1} = {num2}");
+    Console.WriteLine($"{num1_1} = {num1_2}");
 }; */
 
 /* Задача 4: 
@@ -25,7 +25,7 @@ if (num1 > num2) {
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22 */
-
+/*
 Console.WriteLine("Please, input first number:");
 int num2_1 = Convert.ToInt32(Console.ReadLine());
 
@@ -44,13 +44,22 @@ if (num2_1 > num2_2 && num2_1 > num2_3) {
 } if (num2_1 == num2_2 || num2_2 == num2_3 || num2_3 == num2_1) {
     Console.WriteLine("The numbers need to be different");
 }; 
-
+*/
 /* Задача 6: 
 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
 7 -> нет */
 
+Console.WriteLine("Please, input your number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int res = num3 % 2;
+
+if (res == 0) {
+    Console.WriteLine($"{num3} is even"); 
+} else {
+    Console.WriteLine($"{num3} is odd"); 
+};
 
 /* Задача 8: 
 Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
