7411241dea470efd5e7066b6a3f88d2e7da36126
gamba <iamoldandiam16@gmail.com>
2023-06-15 23:42:37 -0500

    added a few new commands

--- Seminars/Seminar1/Program.cs
+++ Seminars/Seminar1/Program.cs
@@ -1,2 +1,8 @@
 ﻿
-Console.WriteLine("Hello, World!");
+Console.WriteLine("What is the date?"); //write on line then enter new line
+// Console.Write("Today is Friday!"); // write on line
+
+// Console.ReadLine(); // - string
+int numA = Convert.ToInt32(Console.ReadLine()); // - int
+Console.WriteLine("Today is " + numA);
+Console.Write($"String is {numA}"); // $" {}" - symbol that allows us to include a number in our string w/o changing the type
