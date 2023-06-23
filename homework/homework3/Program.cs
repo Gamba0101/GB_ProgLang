c0db13e5a711b56ae2d90ca7dd540d74cd43271d
gamba <iamoldandiam16@gmail.com>
2023-06-23 05:11:23 -0500

    added hw file

---
+++ homework/homework3/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.WriteLine("EXERCISE 19");
+
+Console.WriteLine("Please, input your 5 digit number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+Console.WriteLine("Yes, your number is палиндром");
+
+/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */
+
+Console.WriteLine("EXERCISE 21");
+
+
+
+/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.WriteLine("EXERCISE 15");
+
+Console.WriteLine("Please, input your 5 digit number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
