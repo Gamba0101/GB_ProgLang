7dda036b86e90855997c7e1d100613acf8832c7f
gamba <iamoldandiam16@gmail.com>
2023-06-22 23:35:42 -0500

    started eminar

---
+++ Seminars/Seminar2/Program.cs
@@ -0,0 +1,15 @@
+﻿// finding the biggest random number from [10,99] and shows its maximum number
+
+int Line() {
+    int num = new Random().Next(10,99);
+    Console.WriteLine("Your number is " + num);
+    int dec = num/10;
+    int yet = num % 10;
+    if(dec > yet){
+        return dec;
+    } else {
+        return yet;
+    }
+};
+
+Console.WriteLine("Result is - " + Line());
