591199c6183ed4eabb47bae9a0ce7d0c8f44ca6d
gamba <iamoldandiam16@gmail.com>
2023-06-23 05:50:31 -0500

    finished first ex

---
+++ Seminars/Seminar3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+// void - Невозвратный метод
+void NameFunk() { //NameFunk - название; () - аргументы (им не необходимо быть) - значения, которая программа берет извне
+    body;
+}; 
+
+NameFunk(); // вызов метода (он не вернет результат)
+*/
+
+// Напишите прог которая на вход принимает координаты точки (Х & Y)
+// и выводит номер в какой четверти находится координата
+
+int Koord (int X, int Y){
+    int numQuarter;
+    if (X > 0 && Y > 0)
+        numQuarter = 1;
+    else if (X < 0 && Y > 0)
+        numQuarter = 2;
+    else if (X < 0 && Y < 0)
+        numQuarter = 3;
+    else 
+        numQuarter = 4;
+
+    return numQuarter;
+
+};
+
+Console.WriteLine("Input your X coord (they cannot equal 0)");
+int xCoord = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input your Y coord (they cannot equal 0)");
+int yCoord = Convert.ToInt32(Console.ReadLine());
+
+if (xCoord == 0 || yCoord == 0) {
+    Console.WriteLine("ERROR");
+} else {
+int numQuart = Koord(xCoord, yCoord);
+Console.WriteLine($"Your coords are in {numQuart} quarter");
+}
