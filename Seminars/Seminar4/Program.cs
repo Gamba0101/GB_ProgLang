1f13ee7e46586ed27bd7fb559fa4f41cb6516bdd
gamba <iamoldandiam16@gmail.com>
2023-06-26 17:28:52 -0500

    finished first ex

--- Seminars/Seminar4/Program.cs
+++ Seminars/Seminar4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void SumOfNum (int arg) {
+    int Sum = 0;
+    while (arg > 0) {
+        Sum = Sum + arg;
+        arg--;
+    }
+    Console.WriteLine(Sum);
+}
+
+Console.WriteLine("Input your num");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0){
+    SumOfNum(num);
+} else Console.WriteLine("ERROR: input a positive number.");
+
+/* alternitive way: through a FOR cicle 
+
+void SumOfNum (int arg) {
+    int Sum = 0;
+    for (int i = 1; i <= num_a; i++) {
+        Sum += i; // sum = sum + i
+    }
+    Console.WriteLine(Sum);
+}
+
+*/
