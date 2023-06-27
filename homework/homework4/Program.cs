02e2fe13be90bc84b95885f3a86abfdfc666d142
gamba <iamoldandiam16@gmail.com>
2023-06-26 19:15:15 -0500

    finished hw

--- homework/homework4/Program.cs
+++ homework/homework4/Program.cs
@@ -2,6 +2,7 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 
+
 Console.WriteLine("EXERCISE 25");
 
 Console.WriteLine("Input your first number: ");
@@ -24,10 +25,12 @@ if (num1_2 <= 0) Console.WriteLine("ERROR: your second number must be more than 
 else if (num1_1 <= 0) Console.WriteLine("ERROR: your first number must be more than 0.");
 else Exponentiate(num1_1, num1_2);
 
+
 /* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 452 -> 11
 82 -> 10
 9012 -> 12 */
+
 
 Console.WriteLine(" ");
 Console.WriteLine("EXERCISE 27");
@@ -35,6 +38,19 @@ Console.WriteLine("EXERCISE 27");
 Console.WriteLine("Input your num: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
+int DigitCount (int arg) {
+    int i = 0;
+    int res = 0;
+    while (arg > 0 ){
+        res = res + arg%10;
+        arg = arg / 10;
+        i++;
+    }
+    return res;
+} 
+
+if (num2 > 0) Console.WriteLine($"The sum of all the digits in {num2} equals to {DigitCount(num2)}");
+else Console.WriteLine("ERROR: Your number must be more than 0.");
 
 
 /* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
@@ -44,8 +60,22 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(" ");
 Console.WriteLine("EXERCISE 29");
 
-Console.WriteLine("Input your num: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+int size = new Random().Next(3,16);
 
-// int[] array = new int[8];
-// int num = new Random().Next(1,100); 
+int[] CreateArray (int size){
+    int[] temp_ar = new int[size];
+    for (int i = 0; i < size; i++) {
+        temp_ar[i] = new Random().Next(1,100);
+    }
+    return temp_ar;
+}
+
+void PrintArray (int[] print_ar) {
+    Console.WriteLine("Your array is: ");
+    for (int i = 0; i < print_ar.Length; i++) {
+        Console.Write(print_ar[i] + " ");
+    }
+}
+
+int[] firstArray = CreateArray(size);
+PrintArray(firstArray);
