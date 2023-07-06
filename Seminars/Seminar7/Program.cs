7bd4ef0147d2b4ad0d45c75f5f2b67ee6b8ff53f
gamba <iamoldandiam16@gmail.com>
2023-07-05 23:06:30 -0500

    tweaked ex3

--- Seminars/Seminar7/Program.cs
+++ Seminars/Seminar7/Program.cs
@@ -73,7 +73,10 @@ double[,] CreateSquaredArray () {
 double [,] ModifyArray (double[,] array) {
     for (int i = 0; i < array.GetLength(0); i++) {
         for(int j = 0; j < array.GetLength(1); j++) {
-            if ( i%2 == 0 && j%2 == 0) array[i,j] *= array[i,j];
+            if ( i%2 == 0 && j%2 == 0) {
+            array[i,j] *= array[i,j];
+            array[i,j] = Math.Round(array[i,j], 4);
+            }
         }
     }
     return array;
