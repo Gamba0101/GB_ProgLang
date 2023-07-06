6afeb441f87c37bb2c8a7f28d249cdf25be83f9e
gamba <iamoldandiam16@gmail.com>
2023-07-05 23:05:01 -0500

    ex 3 is DONE

--- Seminars/Seminar7/Program.cs
+++ Seminars/Seminar7/Program.cs
@@ -1,6 +1,5 @@
 ﻿// задайте двумерный массив размером mxn, заполненый случайными числамию
 
-// int[,] array = new(x,y); двумерный массив
 /*
 int[,] Create2DArray () {
     Console.WriteLine("Enter count of rows: ");
@@ -19,13 +18,11 @@ int[,] Create2DArray () {
             new2DArray[i,j] = new Random().Next(min,max + 1);
         }
     }
-
     return new2DArray;
-
 }
 */
 
-void Print2DArray (int[,] ar2D) {
+void Print2DArray (double[,] ar2D) {
     for (int i = 0; i < ar2D.GetLength(0); i++) {
         for(int j = 0; j < ar2D.GetLength(1); j++) {
             Console.Write(ar2D[i,j] + "  ") ;
@@ -36,7 +33,7 @@ void Print2DArray (int[,] ar2D) {
 }
 
 // Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле А = m+n
-
+/*
 int[,] FillArray (){
     Console.WriteLine("Enter count of rows: ");
     int mRows = Convert.ToInt32(Console.ReadLine());
@@ -52,6 +49,37 @@ int[,] FillArray (){
     }
     return newArray;
 }
+*/
 
-int[,] new2DArray = FillArray();
-Print2DArray(new2DArray);
+// Задайте двумерный вещественный массив, найдите элементы у которых оба индекса четные и замените эти эл на их квадраты
+
+double[,] CreateSquaredArray () {
+    Console.WriteLine("Enter count of rows: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter count of collums: ");
+    int collums = Convert.ToInt32(Console.ReadLine());
+
+    double[,] new2DArray = new double [rows, collums];
+
+    for (int i = 0; i < rows; i++) {
+        for(int j = 0; j < collums; j++) {
+            new2DArray[i,j] = Math.Round((new Random().NextDouble()), 2);
+        }
+    }
+    return new2DArray;
+
+}
+
+double [,] ModifyArray (double[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for(int j = 0; j < array.GetLength(1); j++) {
+            if ( i%2 == 0 && j%2 == 0) array[i,j] *= array[i,j];
+        }
+    }
+    return array;
+}
+
+double [,] array3 = CreateSquaredArray();
+Print2DArray(array3);
+double [,] modifiedArray3 = ModifyArray(array3);
+Print2DArray(modifiedArray3);
