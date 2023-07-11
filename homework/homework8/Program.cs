123740b95413905393bcb1ab728e5f93a40cb5e7
gamba <iamoldandiam16@gmail.com>
2023-07-10 23:51:18 -0500

    started on ex1

--- homework/homework8/Program.cs
+++ homework/homework8/Program.cs
@@ -1,4 +1,21 @@
-﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿int [,] Create2dArray (int row, int collumn) {
+    int[,] createdArray = new int[row,collumn];
+    for (int i =0; i < row; i++) 
+        for (int j = 0; j < collumn; j++) 
+        createdArray[i,j] = new Random().Next(10, 21);
+    return createdArray;
+}
+
+void Print2DArray (int[,] array) {
+    for (int i = 0; i < array.GetLength(0); i++) {
+        for(int j = 0; j < array.GetLength(1); j++) {
+            Console.Write(array[i,j] + "  ") ;
+        }
+        Console.WriteLine(); 
+    }
+}
+
+/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
@@ -6,36 +23,84 @@
 В итоге получается вот такой массив:
 7 4 2 1
 9 5 3 2
-8 4 4 2
+8 4 4 2 */
 
-Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Console.WriteLine("EXERCISE 54");
 
+Console.WriteLine("Input count of rows: ");
+int rows1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input count of collumns: ");
+int cols1 = Convert.ToInt32(Console.ReadLine());
+
+int[,] array1 = Create2DArray(rows1, cols1);
+Print2DArray(array1);
+
+/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
 8 4 2 4
 5 2 6 7
-Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
 
-Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Console.WriteLine(" ");
+Console.WriteLine("EXERCISE 56");
+
+Console.WriteLine("Input count of rows: ");
+int rows2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input count of collumns: ");
+int cols2 = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2 = Create2DArray(rows2, cols2);
+Print2DArray(array2);
+
+/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 Например, даны 2 матрицы:
 2 4 | 3 4
 3 2 | 3 3
 Результирующая матрица будет:
 18 20
-15 18
+15 18 */
 
-Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Console.WriteLine(" ");
+Console.WriteLine("EXERCISE 58");
+
+Console.WriteLine("Input count of rows: ");
+int rows3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input count of collumns: ");
+int cols3 = Convert.ToInt32(Console.ReadLine());
+
+int[,] array3 = Create2DArray(rows3, cols3);
+Print2DArray(array3);
+
+/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 Массив размером 2 x 2 x 2
 66(0,0,0) 25(0,1,0)
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
-26(1,0,1) 55(1,1,1)
+26(1,0,1) 55(1,1,1) */
 
-Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Console.WriteLine(" ");
+Console.WriteLine("EXERCISE 60");
+
+Console.WriteLine("Input count of rows: ");
+int rows4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input count of collumns: ");
+int cols4 = Convert.ToInt32(Console.ReadLine());
+
+int[,] array4 = Create2DArray(rows4, cols4);
+Print2DArray(array4);
+
+/* Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 Например, на выходе получается вот такой массив:
 01 02 03 04
 12 13 14 05
 11 16 15 06
 10 09 08 07
 */
+
+Console.WriteLine(" ");
+Console.WriteLine("EXERCISE 62");
+
+int[,] array5 = Create2DArray(4, 4);
+Print2DArray(array5);
