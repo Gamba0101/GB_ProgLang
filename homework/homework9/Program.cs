41641d80f7b5d7f68761c7747c809d64c989c59d
gamba <iamoldandiam16@gmail.com>
2023-07-13 16:06:02 -0500

    finished last hw o7

--- homework/homework9/Program.cs
+++ homework/homework9/Program.cs
@@ -22,15 +22,15 @@ Console.WriteLine(" ");
 Console.WriteLine(" ");
 Console.WriteLine("Задача 66");
 
-static int CountRange(int a, int b) {
-        if (a == b) {
-            return 1; 
-        } else if (a > b) {
-            return b + CountRange(a, b + 1); 
-        } else {
-            return a + CountRange(a + 1, b);
-        }
+int CountRange(int a, int b) {
+    if (a == b) {
+        return 1; 
+    } else if (a > b) {
+        return b + CountRange(a, b + 1); 
+    } else {
+        return a + CountRange(a + 1, b);
     }
+}
 
 Console.WriteLine("Enter your first num: ");
 int num2_1 = Convert.ToInt32(Console.ReadLine());
@@ -43,3 +43,18 @@ Console.WriteLine($"The sum of all the numbers between {num2_1} and {num2_2} is 
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
 
+Console.WriteLine(" ");
+Console.WriteLine("Задача 68");
+
+int Ackermann(int m, int n) {
+    if (m == 0)   return n + 1;
+    else if (n == 0)    return Ackermann(m - 1, 1);
+    else    return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine("Enter your first num: ");
+int num3_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter your second num: ");
+int num3_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" ");
+Console.WriteLine($"A({num3_1},{num3_2}) = {Ackermann(num3_1, num3_2)}");
