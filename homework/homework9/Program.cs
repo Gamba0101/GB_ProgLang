b9c1d1e220e26c2b6c04ffd7640a1df144c48732
gamba <iamoldandiam16@gmail.com>
2023-07-13 14:25:42 -0500

    did ex1 and 2

---
+++ homework/homework9/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Задача 64");
+Console.WriteLine(" ");
+
+void ShowNumbers (int n) {
+    Console.Write($"{n} ");
+    if (n > 1) ShowNumbers (n-1);
+}
+
+Console.WriteLine("Enter your num: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(num1);
+Console.WriteLine(" ");
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Задача 66");
+
+int CountRange (int arg1, int arg2) {
+    int sum = 0;
+    sum += Math.Min(arg1,arg2);
+    if (Math.Max(arg1,arg2) != Math.Min(arg1,arg2)) CountRange(Math.Min(arg1, arg2)+1, Math.Max(arg1,arg2));
+    return sum;
+}
+
+Console.WriteLine(CountRange(10,20));
+Console.WriteLine();
+
+// Задача 68*: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
